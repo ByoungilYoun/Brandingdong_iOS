76c714c32c24488c570141b43e851a314379f8b8
jwlee07 <apzk21413@gmail.com>
2020-08-20 15:49:32 +0900

    초기 세팅 - Scene Delegate 삭제 - MainStroyboard 삭제 - info.plist 수정 완료 - MVC 그룹 폴더 생성

---
+++ Brandingdong/Brandingdong/SupportFile/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Brandingdong
+//
+//  Created by 이진욱 on 2020/08/20.
+//  Copyright © 2020 jwlee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.backgroundColor = .systemBackground
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+    return true
+  }
+}
+
